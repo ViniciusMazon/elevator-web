237f5305369bf0e456d9804aefedc787c7155dd8
Vinicius Mazon <vnpmazon@gmail.com>
2021-01-17 15:48:09 -0300

    feat: add Landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,11 +1,81 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 
-import { Container } from './styles';
+import smallLogo from '../../assets/just_logo.svg';
+import logo from '../../assets/logo.svg';
+import {
+  Container,
+  Header,
+  Content,
+  Features,
+  FeaturesGroup,
+  CheckIcon,
+  CompanyIcon,
+  TerminalIcon,
+} from './styles';
 
 export default function Landing() {
+  function handleNavigateToSingIn() {
+    return;
+  }
+
   return (
     <Container>
-      <h1>Landing</h1>
+      <Header>
+        <img src={smallLogo} alt="Elevator logo" />
+        <button onClick={handleNavigateToSingIn}>Entrar</button>
+      </Header>
+      <Content>
+        <img src={logo} alt="Elevator logo" />
+        <p>
+          O Elevator é uma plataforma de recrutamento para profissionais de
+          tecnologia (Desenvolvedores e Designers). Utilizamos um pitch de 1 min
+          como principal meio do candidato se apresentar para vaga, essa
+          abordagem permite agilizar o recrutamento, e ao mesmo tempo, o
+          recrutador consegue avaliar algumas softskills antes mesmo da
+          entrevista.
+        </p>
+
+        <Features>
+          <FeaturesGroup>
+            <li>
+              <CompanyIcon />
+              <p>Empresa</p>
+            </li>
+            <li>
+              <CheckIcon />
+              <p>Torna o processo de recrutamento mais transparente</p>
+            </li>
+            <li>
+              <CheckIcon />
+              <p>Agiliza a análise dos candidatos</p>
+            </li>
+            <li>
+              <CheckIcon />
+              <p>Filtra as propostas</p>
+            </li>
+          </FeaturesGroup>
+          <FeaturesGroup>
+            <li>
+              <TerminalIcon />
+              <p>Desenvolvedores e Designers</p>
+            </li>
+            <li>
+              <CheckIcon />
+              <p>Todas propostas possuem o salário no anúncio</p>
+            </li>
+            <li>
+              <CheckIcon />
+              <p>Elimina o uso de curriculum</p>
+            </li>
+            <li>
+              <CheckIcon />
+              <p>
+                Oportunidades condizentes com sua experiência e preferências
+              </p>
+            </li>
+          </FeaturesGroup>
+        </Features>
+      </Content>
     </Container>
   );
 }
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -1,10 +1,141 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { CheckCircle, Terminal, TrendingUp } from '../../styles/Icons';
 
-export const Container = styled.div`
+import backgroundImage from '../../assets/background.png';
+
+export const Container = styled.main`
   width: 100%;
   height: 100%;
 
-  > h1 {
-    color: tomato;
+  background: url(${backgroundImage}) no-repeat center;
+  background-size: cover;
+  padding: 44px 100px 0 100px;
+
+  @media (max-width: 900px) {
+    background: url(${backgroundImage}) no-repeat center fixed;
+    padding: 20px 20px 0 20px;
+    height: fit-content;
   }
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 88px;
+
+  > button {
+    width: 10rem;
+    height: 34px;
+    border: 1px solid var(--green);
+    border-radius: 6px;
+    font: 1rem 500 'Montserrat', sans-serif;
+    color: var(--green);
+    cursor: pointer;
+    outline: none;
+
+    &:hover {
+      background: var(--main-green);
+      color: var(--white);
+      transition: background 0.5s;
+    }
+  }
+`;
+
+export const Content = styled.article`
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
+  > img {
+    width: 26.2rem;
+  }
+
+  > p {
+    width: 80%;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 2rem;
+    line-height: 195.4%;
+    color: var(--white);
+    text-align: center;
+  }
+
+  @media (max-width: 900px) {
+    p {
+      width: 100%;
+    }
+  }
+`;
+
+export const Features = styled.section`
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+
+  ul + ul {
+    margin-left: 30px;
+  }
+
+  @media (max-width: 900px) {
+    flex-direction: column;
+    margin-top: 70px;
+    align-items: center;
+
+    ul + ul {
+      margin-left: 0;
+      margin-top: 30px;
+    }
+  }
+`;
+
+export const FeaturesGroup = styled.ul`
+  max-width: 506px;
+
+  li {
+    display: flex;
+
+    p {
+      font-family: 'Montserrat', sans-serif;
+      font-size: 1.6rem;
+      font-weight: 400;
+      line-height: 28.38px;
+      color: var(--white);
+    }
+    list-style: none;
+    margin-bottom: 15px;
+
+    &:nth-child(1) {
+      > p {
+        font-weight: 700;
+      }
+      margin-bottom: 20px;
+    }
+  }
+`;
+
+const iconStyle = css`
+  width: 2rem;
+  height: 2rem;
+  flex-shrink: 0;
+  stroke: var(--main-green);
+  margin-right: 2.4rem;
+`;
+
+export const CheckIcon = styled(CheckCircle)`
+  ${iconStyle}
+`;
+
+export const TerminalIcon = styled(Terminal)`
+  ${iconStyle}
+  stroke: var(--white);
+  stroke-width: 2px;
+`;
+
+export const CompanyIcon = styled(TrendingUp)`
+  ${iconStyle}
+  stroke: var(--white);
+  stroke-width: 2px;
 `;
