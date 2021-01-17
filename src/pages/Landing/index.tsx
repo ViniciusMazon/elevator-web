1f6997d0d5891351022ca1a3e874b5d5ca7a6d85
Vinicius Mazon <vnpmazon@gmail.com>
2021-01-17 08:39:52 -0300

    chore: add Landing route and Landing page directory

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Landing() {
+  return (
+    <Container>
+      <h1>Landing</h1>
+    </Container>
+  );
+}
