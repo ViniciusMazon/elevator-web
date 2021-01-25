730cac30b5267e72f40b0d093bb4fe5ae1fb885d
Vinicius Mazon <vnpmazon@gmail.com>
2021-01-25 09:06:00 -0300

    feat: update InputText component
    Add value and changeValue props

--- src/components/InputText/index.tsx
+++ src/components/InputText/index.tsx
@@ -3,15 +3,27 @@ import React from 'react';
 interface InputTextData {
   label: string;
   placeholder: string;
+  value: string;
+  changeValue: React.Dispatch<React.SetStateAction<string>>;
 }
 
 import { Container, Label } from './styles';
 
-const InputText = ({ label, placeholder }: InputTextData) => {
+const InputText = ({
+  label,
+  placeholder,
+  value,
+  changeValue,
+}: InputTextData) => {
   return (
     <Container>
       <Label>{label}</Label>
-      <input type="text" placeholder={placeholder} />
+      <input
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => changeValue(e.target.value)}
+      />
     </Container>
   );
 };
